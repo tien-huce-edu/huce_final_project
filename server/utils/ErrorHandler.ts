b757d090bd0b8c661b50f10317bf9fba3525b94c
tienhg1310 <tienhg201@gmail.com>
2024-01-27 10:33:05 +0700

    reflactor: reflactor and change syntax

--- server/utils/ErrorHandler.ts
+++ server/utils/ErrorHandler.ts
@@ -1,6 +1,6 @@
 class ErrorHandler extends Error {
-    statusCode: Number
-    constructor(massage: any, statusCode: Number) {
+    statusCode: number
+    constructor(massage: any, statusCode: number) {
         super(massage)
         this.statusCode = statusCode
         Error.captureStackTrace(this, this.constructor)
