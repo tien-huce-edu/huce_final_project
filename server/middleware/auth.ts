aa0e870886530b87ee235f362f94bf7bae72cbc4
tienhg1310 <tienhg201@gmail.com>
2024-01-28 01:12:36 +0700

    reflac: change st in redis

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -6,16 +6,16 @@ import { redis } from "../utils/redis"
 
 export const isAuthenticated = catchAsyncError(
     async (req: Request, res: Response, next: NextFunction) => {
-        const access_token = req.cookies.access_token
+        const access_token = req.cookies.access_token as string
         if (!access_token) {
-            return next(new ErrorHandler("Bạn chưa đăng nhập", 401))
+            return next(new ErrorHandler("Cookie hết hạn!", 401))
         }
         const decoded = jwt.verify(
             access_token,
             process.env.ACCESS_TOKEN as string
         ) as JwtPayload
         if (!decoded) {
-            return next(new ErrorHandler("Phiên đăng nhập không tồn tại!", 401))
+            return next(new ErrorHandler("Lỗi máy chủ vui lòng thử lại!", 500))
         }
         const user = await redis.get(decoded.id)
         if (!user) {
