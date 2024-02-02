2b5085f5651571700631c53ee68d4b7b2e67cf13
tienhg1310 <tienhg201@gmail.com>
2024-02-02 16:13:10 +0700

    feat: update user info , update password and update profile picture

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -6,7 +6,10 @@ import {
     logoutUser,
     registrationUser,
     socialAuth,
-    updateAccessToken
+    updateAccessToken,
+    updatePassword,
+    updateProfilePicture,
+    updateUserInfo
 } from "../controllers/users.controller"
 import { isAuthenticated } from "../middleware/auth"
 
@@ -19,5 +22,8 @@ userRouter.post("/logout", isAuthenticated, logoutUser)
 userRouter.get("/refresh-token", updateAccessToken)
 userRouter.get("/me", isAuthenticated, getUserInfo)
 userRouter.post("/social-auth", socialAuth)
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo)
+userRouter.put("/update-user-password", isAuthenticated, updatePassword)
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture)
 
 export default userRouter
--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,11 +1,19 @@
 // get user by id
 import { Response } from "express"
-import userModel from "../models/user.model"
+import { redis } from "../utils/redis"
 
 export const getUserById = async (id: string, res: Response) => {
-    const user = await userModel.findById(id)
-    res.status(201).json({
-        success: true,
-        user
-    })
+    // const user = await userModel.findById(id)
+    // res.status(201).json({
+    //     success: true,
+    //     user
+    // })
+    const userJson = await redis.get(id)
+    if (userJson) {
+        const user = JSON.parse(userJson)
+        res.status(201).json({
+            success: true,
+            user
+        })
+    }
 }
