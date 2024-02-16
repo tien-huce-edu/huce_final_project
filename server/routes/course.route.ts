5f7b7e0b9d8496eee6501fb6329212ec9c9cd1a6
Tien Nguyen <tien195164@huce.edu.vn>
2024-02-16 23:23:04 +0700

    feat/get_single_and_all_courses

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,10 +1,14 @@
 import express from "express"
-import { editCourse, uploadCourse } from "../controllers/course.controller"
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controller"
 import { authorizeRoles, isAuthenticated } from "../middleware/auth"
 const courseRouter = express.Router()
 
 courseRouter.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse)
 
 courseRouter.put("/edit-course/:id", isAuthenticated, authorizeRoles("admin"), editCourse)
+
+courseRouter.get("/get-course/:id", getSingleCourse)
+
+courseRouter.get("/get-course", getAllCourses)
 
 export default courseRouter
