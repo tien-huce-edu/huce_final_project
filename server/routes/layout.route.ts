f24d92e979202f2e24f495e9754a193f66d2b1a0
Tien Nguyen <tien195164@huce.edu.vn>
2024-04-19 10:23:00 +0700

    [feature/code]-[add update accesstoken for route needed it]

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,13 +1,32 @@
 import express from "express"
 import { authorizeRoles, isAuthenticated } from "../middleware/auth"
 import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller"
+import { updateAccessToken } from "../controllers/users.controller"
 
 const layoutRouter = express.Router()
 
-layoutRouter.post("/create-layout", isAuthenticated, authorizeRoles("admin"), createLayout)
+layoutRouter.post(
+    "/create-layout",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    createLayout
+)
 
-layoutRouter.put("/edit-layout", isAuthenticated, authorizeRoles("admin"), editLayout)
+layoutRouter.put(
+    "/edit-layout",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    editLayout
+)
 
-layoutRouter.get("/get-layout", isAuthenticated, authorizeRoles("admin"), getLayoutByType)
+layoutRouter.get(
+    "/get-layout",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    getLayoutByType
+)
 
 export default layoutRouter
--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -2,10 +2,11 @@ import { getAllOrders } from "./../controllers/order.controller"
 import express from "express"
 import { createOrder } from "../controllers/order.controller"
 import { authorizeRoles, isAuthenticated } from "../middleware/auth"
+import { updateAccessToken } from "../controllers/users.controller"
 const orderRouter = express.Router()
 
 orderRouter.post("/create-order", isAuthenticated, createOrder)
 
-orderRouter.get("/get-orders", isAuthenticated, authorizeRoles("admin"), getAllOrders)
+orderRouter.get("/get-orders",updateAccessToken, isAuthenticated, authorizeRoles("admin"), getAllOrders)
 
 export default orderRouter
