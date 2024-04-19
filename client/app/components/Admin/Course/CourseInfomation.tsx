fcd2f18ff7a051848446f67ff73f26112d0350cf
Tien Nguyen <tien195164@huce.edu.vn>
2024-04-19 11:26:58 +0700

    [feature/reflactor]-['use client' in client]

--- client/app/components/Admin/Course/CourseInfomation.tsx
+++ client/app/components/Admin/Course/CourseInfomation.tsx
@@ -1,5 +1,5 @@
+"use client"
 import { styles } from "@/app/styles/style";
-import { read } from "fs";
 import { FC, useState } from "react";
 import { IoMdArrowDropdownCircle } from "react-icons/io";
 
--- client/app/components/Profile/ProfileInfo.tsx
+++ client/app/components/Profile/ProfileInfo.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React, { FC, useEffect, useState } from "react";
 import { AiOutlineCamera } from "react-icons/ai";
