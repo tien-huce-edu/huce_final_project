add1cad66f59a62b13d953d57f1ed222ee2ec03f
Tien Nguyen <tien195164@huce.edu.vn>
2024-05-04 14:09:49 +0700

    custom hero page

---
+++ client/app/admin/hero/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import AdminSidebar from "@/app/components/Admin/Sidebar/AdminSidebar";
+import Heading from "@/app/utils/Heading";
+import CreateCourse from "@/app/components/Admin/Course/CreateCourse";
+import DashboardHeader from "@/app/components/Admin/DashboardHeader";
+import EditHero from "../../components/Admin/Customization/EditHero";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Heading
+        title="Elearning - Admin"
+        description="Elearning là một nền tảng dành cho sinh viên và giảng viên trao đổi việc giảng dạy và học tập online"
+        keywords="Programing, Elearning, Online, Study, Teaching, Learning"
+      />
+      <div className="flex">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSidebar />
+        </div>
+        <div className="w-[85%]">
+          <DashboardHeader />
+          {/* <CreateCourse /> */}
+          <EditHero />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
