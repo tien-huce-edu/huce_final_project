d96aba77751ff38b9be3aafd3b490105a3659d2c
Tien Nguyen <tien195164@huce.edu.vn>
2024-05-23 16:12:49 +0700

    feature/code_fix-bug-logout-and-ui

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -6,11 +6,12 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { FC, useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import Loader from "../Loader/Loader";
 
 type Props = {};
 
 const Hero: FC<Props> = (props) => {
-  const { data, refetch } = useGetHeroDataQuery("Banner", {});
+  const { data, isLoading } = useGetHeroDataQuery("Banner", {});
   const [search, setSearch] = useState("");
   const router = useRouter();
 
@@ -23,79 +24,83 @@ const Hero: FC<Props> = (props) => {
   };
   return (
     <>
-      <div className="w-full h-full 1000px:flex items-center">
-        <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[600px] 1500px:w-[600px] 1100px:h-[500px] 1100px:w-[600px] h-[50vh]  w-[100vw] hero_animation rounded-[50%] 1100px:left-8 1500px:left-[1rem]"></div>
-        <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
-          <Image
-            src={data?.layout?.banner?.image?.url}
-            width={400}
-            height={400}
-            alt=""
-            className="object-contain w-[90%] 1100px:max-w-[90%]  1500px:max-w-[85%] h-[auto] z-[10]"
-          />
-        </div>
-        <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-          <h2 className="dark:text-white text-[#000000c7] text-[20px] px-3 w-full 1000px:text-[60px] font-[200] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%]">
-            {data?.layout?.banner.title}
-          </h2>
-          <br />
-          <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
-            {data?.layout?.banner.subTitle}
-          </p>
-          <br />
-          <br />
-          <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
-            <input
-              type="search"
-              placeholder="Tìm kiếm khoá học..."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full h-full 1000px:flex items-center">
+          <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[600px] 1500px:w-[600px] 1100px:h-[500px] 1100px:w-[600px] h-[50vh]  w-[100vw] hero_animation rounded-[50%] 1100px:left-8 1500px:left-[1rem]"></div>
+          <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
+            <Image
+              src={data?.layout?.banner?.image?.url}
+              width={400}
+              height={400}
+              alt=""
+              className="object-contain w-[90%] 1100px:max-w-[90%]  1500px:max-w-[85%] h-[auto] z-[10]"
             />
-            <div
-              className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]"
-              onClick={handleSearch}
-            >
-              <BiSearch className="text-white" size={30} />
-            </div>
           </div>
-          <br />
-          <br />
-          <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
-            <Image
-              src="https://randomuser.me/api/portraits/women/2.jpg"
-              width={50}
-              height={50}
-              alt=""
-              className="rounded-full"
-            />
-            <Image
-              src="https://randomuser.me/api/portraits/men/1.jpg"
-              width={50}
-              height={50}
-              alt=""
-              className="rounded-full ml-[-20px]"
-            />
-            <Image
-              src="https://randomuser.me/api/portraits/women/3.jpg"
-              width={50}
-              height={50}
-              alt=""
-              className="rounded-full ml-[-20px]"
-            />
-            <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]">
-              500K+ người đã tin tưởng chúng tôi.{" "}
-              <Link
-                href="/courses"
-                className="dark:text-[#46e256] text-[crimson]"
-              >
-                Xem khoá học
-              </Link>{" "}
+          <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
+            <h2 className="dark:text-white text-[#000000c7] text-[20px] px-3 w-full 1000px:text-[60px] font-[200] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%]">
+              {data?.layout?.banner.title}
+            </h2>
+            <br />
+            <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
+              {data?.layout?.banner.subTitle}
             </p>
+            <br />
+            <br />
+            <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
+              <input
+                type="search"
+                placeholder="Tìm kiếm khoá học..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
+              />
+              <div
+                className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]"
+                onClick={handleSearch}
+              >
+                <BiSearch className="text-white" size={30} />
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
+              <Image
+                src="https://randomuser.me/api/portraits/women/2.jpg"
+                width={50}
+                height={50}
+                alt=""
+                className="rounded-full"
+              />
+              <Image
+                src="https://randomuser.me/api/portraits/men/1.jpg"
+                width={50}
+                height={50}
+                alt=""
+                className="rounded-full ml-[-20px]"
+              />
+              <Image
+                src="https://randomuser.me/api/portraits/women/3.jpg"
+                width={50}
+                height={50}
+                alt=""
+                className="rounded-full ml-[-20px]"
+              />
+              <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]">
+                500K+ người đã tin tưởng chúng tôi.{" "}
+                <Link
+                  href="/courses"
+                  className="dark:text-[#46e256] text-[crimson]"
+                >
+                  Xem khoá học
+                </Link>{" "}
+              </p>
+            </div>
+            <br />
           </div>
-          <br />
         </div>
-      </div>
+      )}
     </>
   );
 };
